373061b1b360da06dbbfa7dd1c6f60f9e0ca829b
nikita-parkhomenko <nikita.parkhom@gmail.com>
2021-07-06 19:30:27 +0300

    add header component

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import classes from './Header.module.css';
+import leSilpoImage from '../../assets/lesilpo_banner.jpg';
+
+const Header = props => {
+    return <>
+        <header className={classes.header}>
+            <h1>Le Silpo Meals</h1>
+            <button>Cart</button>
+        </header>
+        <div className={classes["main-image"]}>
+            <img src={leSilpoImage} alt="brand banner" />
+        </div>
+    </>
+}
+
+export default Header;
