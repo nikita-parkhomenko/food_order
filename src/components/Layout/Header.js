02cc266ed378bc195655d5819abdd34d1da37d6d
nikita-parkhomenko <nikita.parkhom@gmail.com>
2021-07-07 10:49:25 +0300

    add HeaderCartButton component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,11 +3,13 @@ import React from 'react';
 import classes from './Header.module.css';
 import leSilpoImage from '../../assets/silpo_banner.jpg';
 
+import HeaderCartButton from './HeaderCartButton';
+
 const Header = props => {
     return <>
         <header className={classes.header}>
             <h1>Le Silpo Meals</h1>
-            <button>Cart</button>
+            <HeaderCartButton />
         </header>
         <div className={classes["main-image"]}>
             <img src={leSilpoImage} alt="brand banner" />
